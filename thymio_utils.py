a6671525621fd39e13104c3c3568c2e5ced6a1b1
Joshua Koroluk <Joshua.Koroluk@simplution.ch>
2022-10-17 23:18:14 +0200

    josh pls

--- thymio_utils.py
+++ thymio_utils.py
@@ -1,5 +1,5 @@
 PROXIMITY_FRONT_BACK = "prox.horizontal"
-PROXIMITY_GROUND_AMBIENT = "prox.ground.ambiant"
+PROXIMITY_GROUND_AMBIANT = "prox.ground.ambiant"
 PROXIMITY_GROUND_REFLECTED = "prox.ground.reflected"
 PROXIMITY_GROUND_DELTA = "prox.ground.delta"
 
