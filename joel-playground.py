5da6f52cd3cecb8cc79dc02772fcec9c92dfb140
Joel L <joelius300@protonmail.com>
2022-10-18 10:54:22 +0200

    Implement framework for simple runner

--- joel-playground.py
+++ joel-playground.py
@@ -1,74 +1,42 @@
-import os
+from thymio_utils import BUTTON_CENTER, PROXIMITY_GROUND_AMBIENT, PROXIMITY_GROUND_REFLECTED, PROXIMITY_GROUND_DELTA, \
+    PROXIMITY_FRONT_BACK, MOTOR_LEFT, MOTOR_RIGHT, ThymioObserver, print_thymio_functions_events, \
+    SingleSerialThymioRunner
 
-from thymio_python.thymiodirect import Connection, Thymio
-from thymio_python.thymiodirect.thymio_serial_ports import ThymioSerialPort
-import sys
-import time
-from thymio_utils import BUTTON_CENTER, PROXIMITY_GROUND_AMBIENT, PROXIMITY_GROUND_REFLECTED, PROXIMITY_GROUND_DELTA, PROXIMITY_FRONT_BACK
+
+class HandAvoider(ThymioObserver):
+
+    def __init__(self):
+        super().__init__()
+        self.prox_prev = None
+
+    def _update(self):
+        prox = (self.th[PROXIMITY_FRONT_BACK][5] - self.th[PROXIMITY_FRONT_BACK][2]) // 10
+        print(self.th[PROXIMITY_GROUND_AMBIENT])
+
+        if prox != self.prox_prev:
+            self.th[MOTOR_LEFT] = prox
+            self.th[MOTOR_RIGHT] = prox
+            print(prox)
+            if prox > 5:
+                self.th["leds.top"] = [0, 32, 0]
+            elif prox < -5:
+                self.th["leds.top"] = [32, 32, 0]
+            elif abs(prox) < 3:
+                self.th["leds.top"] = [0, 0, 32]
+            self.prox_prev = prox
+        if self.th[BUTTON_CENTER]:
+            print("Center button pressed")
+            self.stop()
+
+
+# for straight and sightly curved lines without intersections
+# class ScaledAdjustmentLineDriver(ThymioObserver):
+
 
 if __name__ == "__main__":
-    def on_error(error):
-        print(error)
-        global done
-        done = True  # should make the waiting loop exit which (thanks to the with) will disconnect the Thymio
+    observer = HandAvoider()
+    runner = SingleSerialThymioRunner({BUTTON_CENTER, PROXIMITY_GROUND_AMBIENT, PROXIMITY_GROUND_REFLECTED,
+                                       PROXIMITY_GROUND_DELTA, PROXIMITY_FRONT_BACK},
+                                      observer)
 
-    th = None
-    try:
-        th = Thymio(refreshing_coverage={BUTTON_CENTER, PROXIMITY_GROUND_AMBIENT, PROXIMITY_GROUND_REFLECTED, PROXIMITY_GROUND_DELTA, PROXIMITY_FRONT_BACK})
-        th.on_comm_error = on_error
-        th.connect()
-    except Exception as error:
-        if th is not None:
-            th.disconnect()
-        print(error)
-        sys.exit(1)
-
-    # wait 2-3 sec until robots are known
-    time.sleep(2)
-
-    with th:
-        id = th.first_node()
-
-        print(f"id: {id}")
-        print(f"variables: {th.variables(id)}")
-        print(f"events: {th.events(id)}")
-        print(f"native functions: {th.native_functions(id)[0]}")
-
-        # get a variable
-        print(f'prox.horizontal: {th[id]["prox.horizontal"]}')
-
-        # set a variable (scalar or array)
-        th[id]["leds.top"] = [0, 0, 32]
-
-        # set a function called after new variable values have been fetched
-        prox_prev = 0
-        done = False
-
-        def obs(node_id):
-            global prox_prev, done
-            prox = (th[node_id]["prox.horizontal"][5] - th[node_id]["prox.horizontal"][2]) // 10
-
-            print(th[node_id][PROXIMITY_GROUND_AMBIENT])
-
-            if prox != prox_prev:
-                th[node_id]["motor.left.target"] = prox
-                th[node_id]["motor.right.target"] = prox
-                print(prox)
-                if prox > 5:
-                    th[id]["leds.top"] = [0, 32, 0]
-                elif prox < -5:
-                    th[id]["leds.top"] = [32, 32, 0]
-                elif abs(prox) < 3:
-                    th[id]["leds.top"] = [0, 0, 32]
-                prox_prev = prox
-            if th[node_id]["button.center"]:
-                print("button.center")
-                done = True
-
-        th.set_variable_observer(id, obs)
-
-        while not done:
-            try:
-                time.sleep(.05)
-            except KeyboardInterrupt:
-                break
+    runner.run()
--- thymio_utils.py
+++ thymio_utils.py
@@ -1,3 +1,11 @@
+from abc import ABC, abstractmethod
+import time
+from typing import Optional
+
+from serial import PortNotOpenError
+
+from thymio_python.thymiodirect import Thymio
+
 PROXIMITY_FRONT_BACK = "prox.horizontal"
 PROXIMITY_GROUND_AMBIENT = "prox.ground.ambiant"
 PROXIMITY_GROUND_REFLECTED = "prox.ground.reflected"
@@ -5,3 +13,90 @@ PROXIMITY_GROUND_DELTA = "prox.ground.delta"
 
 BUTTON_CENTER = "button.center"
 
+MOTOR_LEFT = "motor.left.target"
+MOTOR_RIGHT = "motor.right.target"
+
+
+def print_thymio_functions_events(th: Thymio, node_id: int):
+    print(f"id: {node_id}")
+    print(f"variables: {th.variables(node_id)}")
+    print(f"events: {th.events(node_id)}")
+    print(f"native functions: {th.native_functions(node_id)[0]}")
+
+
+class ThymioObserver(ABC):
+    def __init__(self):
+        self.thymio: Thymio = None
+        self.th: Thymio.Node = None
+        self._node_id: int = None
+        self.done = False
+
+    def set_thymio_node(self, thymio: Thymio, node_id: int):
+        self.thymio = thymio
+        self.th = thymio[node_id]
+        self._node_id = node_id
+
+    def run(self):
+        self.done = False
+        self.thymio.set_variable_observer(self._node_id, self._observe)
+        while not self.done:
+            try:
+                time.sleep(.05)
+            except KeyboardInterrupt:
+                self.done = True
+
+    def _observe(self, node_id):
+        if self.done:
+            return
+
+        assert (node_id == self._node_id)
+        self._update()
+
+    @abstractmethod
+    def _update(self):
+        pass
+
+    def stop(self):
+        self.done = True
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.stop()
+
+
+class SingleSerialThymioRunner:
+    def __init__(self,
+                 refreshing_coverage: set,
+                 observer: ThymioObserver,
+                 refreshing_rate=0.1,
+                 serial_port: Optional[str] = None,
+                 connection_delay=1.5
+                 ):
+        self.thymio = Thymio(refreshing_coverage=refreshing_coverage,
+                             refreshing_rate=refreshing_rate,
+                             serial_port=serial_port,
+                             on_comm_error=lambda e: self._on_error(e),
+                             discover_rate=1)
+        self.observer = observer
+        self._connection_delay = connection_delay
+
+    def _on_error(self, error):
+        print(error)
+        self.observer.stop()
+
+    def run(self):
+        with self.thymio, self.observer:
+            self.thymio.connect()
+
+            time.sleep(self._connection_delay)
+
+            id = self.thymio.first_node()
+            self.observer.set_thymio_node(self.thymio, id)
+
+            try:
+                self.observer.run()  # blocks until end
+            except PortNotOpenError as error:
+                if not self.observer.done:
+                    raise error  # during disconnect, there may be a serial port error but we don't care about that
