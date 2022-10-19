8f4952a64ce1c64569a7a0a5392f9d6e049ada3e
Joshua Koroluk <Joshua.Koroluk@simplution.ch>
2022-10-19 03:20:51 +0200

    made a deal with the devil

--- josh-playground.py
+++ josh-playground.py
@@ -1,96 +1,116 @@
-from thymio_python.thymiodirect import Thymio
-from thymio_python.thymiodirect.thymio_serial_ports import ThymioSerialPort
-import thymio_utils
-import sys
-import os
-import time
+import math
+from thymio_utils import BUTTON_CENTER, MOTOR_LEFT, MOTOR_RIGHT, PROXIMITY_GROUND_DELTA, PROXIMITY_GROUND_REFLECTED, SingleSerialThymioRunner, ThymioObserver
 
+
+class LineFollower(ThymioObserver):
+	def __init__(self):
+		super().__init__()
+		self.step = 0
+		self.speed = 100
+		self.temp_speed = None
+		self.slow_speed = 75
+		self.speed_step = 10
+		self.min_darkness = 50
+		self.max_darkness = 700
+		self.curve_steps = 30
+		self.curve = self.exponential_curve(self.curve_steps, abs(self.speed * 1.2), 20)
+		self.slow_steps_remaining = -1
+		print(self.curve)
+
+	def exponential_curve(self, steps:int, stop:int, idklol:float):
+		base = math.log(steps) / math.log(idklol);
+		points = [math.floor(stop / math.pow(base, i)) for i in range(steps)]
+		points.reverse()
+		return points
+
+	def _update(self):
+		self.step = self.step + 1
+		self.rainbow(self.step)
+		self.follow_the_darkness()
+		self.handle_buttons()
+
+	def set_lights(self, r, g, b):
+			self.th["leds.top"] = [r, g, b]
+
+	def rainbow(self, step):
+		step = step % (6 * 32)
+		i = step % 32
+		if step < 1 * 32:
+			self.set_lights(31, i, 0)
+		elif step < 2 * 32:
+			self.set_lights(31 - i, 31, 0)
+		elif step < 3 * 32:
+			self.set_lights(0, 31, i)
+		elif step < 4 * 32:
+			self.set_lights(0, 31 - i, 31)
+		elif step < 5 * 32:
+			self.set_lights(i, 0, 31)
+		elif step < 6 * 32:
+			self.set_lights(31, 0, 31 - i)
+		
+	def handle_buttons(self):
+		if self.th[BUTTON_CENTER]:
+			print("done")
+			self.th[MOTOR_LEFT] = 0
+			self.th[MOTOR_RIGHT] = 0
+			self.stop()
+		if self.th["button.right"]:
+			self.speed = self.speed + self.speed_step
+			print(self.speed)
+		if self.th["button.left"]:
+			self.speed = self.speed - self.speed_step
+			print(self.speed)
+
+	# WIP
+	# works as intended but intended badly
+	# I think mapping the numbers in a logarithmic way (or exponential idk) would make sense. That wiggle should be minimized
+	def map_steer_to_speed_reduction(self, steer):
+		steer = abs(steer)
+		if steer > self.max_darkness:
+			steer = self.max_darkness
+		ratio = (self.curve_steps - 1) / (self.max_darkness - self.min_darkness)
+		curve_index = math.floor(abs((steer - self.min_darkness) * ratio))
+		return self.curve[curve_index]
+	
+
+	# WIP
+	# THIS IS LITERALLY BORKEN MONKA
+	def adjust_speed(self, slow_speed):
+		if not self.temp_speed:
+			self.temp_speed = self.speed
+			self.speed = slow_speed
+		self.slow_steps_remaining = self.slow_steps_remaining - 1
+		if self.slow_steps_remaining <= 0:
+			self.speed = self.temp_speed
+			self.temp_speed = None
+		return
+
+	def follow_the_darkness(self):
+		# steer towards darkness (== less reflextion), so positive = right, negative = left
+		steer = self.th[PROXIMITY_GROUND_REFLECTED][0] - self.th[PROXIMITY_GROUND_REFLECTED][1]
+		
+		new_speed_inner = self.speed - self.map_steer_to_speed_reduction(steer)
+
+		# This cheap formula works 
+		# new_speed_inner = self.speed - abs(steer // 10)
+
+		if new_speed_inner <= 0:
+			self.slow_steps_remaining = 10
+
+		if self.slow_steps_remaining >= 0:
+			self.adjust_speed(self.slow_speed)
+
+		if steer > self.min_darkness:
+			self.th[MOTOR_RIGHT] = new_speed_inner
+			self.th[MOTOR_LEFT] = self.speed
+		else:
+			self.th[MOTOR_LEFT] = new_speed_inner
+			self.th[MOTOR_RIGHT] = self.speed
+		
 
 if __name__ == "__main__":
-
-		# check arguments
-		use_tcp = False
-		serial_port = None
-		host = None
-		tcp_port = None
-		if len(sys.argv) == 3:
-				# tcp: argv[1] = host, argv[2] = port
-				use_tcp = True
-				host = sys.argv[1]
-				tcp_port = int(sys.argv[2])
-		elif len(sys.argv) == 2:
-				if sys.argv[1] == "--help":
-						print("Usage: {sys.argv[0]} [serial_port | host port]")
-						sys.exit(0)
-				# serial port: argv[1] = serial port
-				serial_port = sys.argv[1]
-
-		# use thymio_serial_ports for default Thymio serial port
-		if not tcp_port and serial_port is None:
-				thymio_serial_ports = ThymioSerialPort.get_ports()
-				if len(thymio_serial_ports) > 0:
-						serial_port = thymio_serial_ports[0].device
-						print("Thymio serial ports:")
-						for thymio_serial_port in thymio_serial_ports:
-								print(" ", thymio_serial_port, thymio_serial_port.device)
-
-		# connect
-		try:
-				th = Thymio(use_tcp=use_tcp,
-										serial_port=serial_port,
-										host=host, tcp_port=tcp_port,
-										refreshing_coverage={thymio_utils.PROXIMITY_GROUND_AMBIANT, thymio_utils.PROXIMITY_GROUND_REFLECTED, thymio_utils.PROXIMITY_GROUND_DELTA},
-										)
-				# constructor options: on_connect, on_disconnect, on_comm_error,
-				# refreshing_rate, refreshing_coverage, discover_rate, loop
-		except Exception as error:
-				print(error)
-				exit(1)
-
-		def on_comm_error(error):
-				# loss of connection: display error and exit
-				print(error)
-				os._exit(1)  # forced exit despite coroutines
-
-		th.on_comm_error = on_comm_error
-
-		th.connect()
-		print("CONNECTIONN")
-
-		# wait 2-3 sec until robots are known
-		id = th.first_node()
-
-		def obs(node_id):
-			print(f'a: {th[id][thymio_utils.PROXIMITY_GROUND_AMBIANT]} | r: {th[id][thymio_utils.PROXIMITY_GROUND_REFLECTED]} | d: {th[id][thymio_utils.PROXIMITY_GROUND_DELTA]}')
-			# print("Ambiend: {}\nDelda: {}\nReflecded: {}".format(th[node_id][thymio_utils.PROXIMITY_GROUND_AMBIENT], th[node_id][thymio_utils.PROXIMITY_GROUND_DELTA], th[node_id][thymio_utils.PROXIMITY_GROUND_REFLECTED]))
-
-		th.set_variable_observer(id, obs)
-		
-		def set_lights(r, g, b):
-			th[id]["leds.top"] = [r, g, b]
-
-		def rainbow(step):
-			step = step % (6 * 32)
-			i = step % 32
-			if step < 1 * 32:
-				set_lights(31, i, 0)
-			elif step < 2 * 32:
-				set_lights(31 - i, 31, 0)
-			elif step < 3 * 32:
-				set_lights(0, 31, i)
-			elif step < 4 * 32:
-				set_lights(0, 31 - i, 31)
-			elif step < 5 * 32:
-				set_lights(i, 0, 31)
-			elif step < 6 * 32:
-				set_lights(31, 0, 31 - i)
-
-		step = 0
-		while True:
-			try:
-				step = step + 1
-				rainbow(step)
-				time.sleep(0.05)
-			except KeyboardInterrupt:
-				break
-		th.disconnect()
+	observer = LineFollower()
+	runner = SingleSerialThymioRunner({BUTTON_CENTER, PROXIMITY_GROUND_REFLECTED, PROXIMITY_GROUND_DELTA},
+																		observer,
+																		0.1)
+	runner.run()
