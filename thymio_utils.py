916c07d4a908200d5e599d97ce1a96a1aad5a401
Joel L <joelius300@protonmail.com>
2022-10-20 15:17:48 +0200

    Document and slightly refactor utils

--- thymio_utils.py
+++ thymio_utils.py
@@ -2,8 +2,6 @@ from abc import ABC, abstractmethod
 import time
 from typing import Optional
 
-from serial import PortNotOpenError
-
 from thymio_python.thymiodirect import Thymio
 
 PROXIMITY_FRONT_BACK = "prox.horizontal"
@@ -25,39 +23,83 @@ def print_thymio_functions_events(th: Thymio, node_id: int):
 
 
 class ThymioObserver(ABC):
+    """
+    Abstract observer to execute code in an update loop after the newest values are fetched.
+    Only one observer can run per node; you have to implement a combined observer to run multiple observers per thymio node.
+    """
     def __init__(self):
         self.thymio: Thymio = None
         self.th: Thymio.Node = None
         self._node_id: int = None
-        self.done = False
+        self._done = False
+
+    @property
+    def done(self) -> bool:
+        """
+        Gets whether the observer is done observing.
+        """
+        return self._done
 
     def set_thymio_node(self, thymio: Thymio, node_id: int):
+        """
+        Sets the thymio node this observer runs on
+        """
         self.thymio = thymio
         self.th = thymio[node_id]
         self._node_id = node_id
 
     def run(self):
-        self.done = False
-        self.thymio.set_variable_observer(self._node_id, self._observe)
-        while not self.done:
-            try:
-                time.sleep(.05)
-            except KeyboardInterrupt:
-                self.done = True
+        """
+        Run the observer until it is stopped or interrupted (blocking).
+        Before running the observer, you need to set the thymio node (set_thymio_node)!
+        """
+        self.start()
+        self.block_until_done()
 
     def _observe(self, node_id):
         if self.done:
             return
 
-        assert (node_id == self._node_id)
+        if node_id != self._node_id:
+            print("Observe called by a node that is not the registered thymio node of this observer!")
+
         self._update()
 
     @abstractmethod
     def _update(self):
+        """
+        Called everytime the values are fetched from the thymio (refreshing_rate configurable on the thymio itself).
+        """
         pass
 
+    def _reset(self):
+        self._done = False
+
+    def start(self):
+        """
+        Registers and enables the observer without blocking (returns immediately).
+        Before starting the observer, you need to set the thymio node (set_thymio_node)!
+        """
+        self._reset()
+        self.thymio.set_variable_observer(self._node_id, self._observe)
+
+    def block_until_done(self):
+        """
+        Blocks until the observer is done (stopped) or interrupted.
+        """
+        while not self.done:
+            try:
+                time.sleep(.05)
+            except KeyboardInterrupt:
+                self.stop()
+
     def stop(self):
-        self.done = True
+        """
+        Stop operation and prevent _update from being called again.
+        This also stops run() and block_until_done() from blocking and makes them return.
+        """
+        self._done = True
+        self.thymio.set_variable_observer(self._node_id, lambda: None)
 
     def __enter__(self):
         return self
@@ -87,6 +129,10 @@ class SingleSerialThymioRunner:
         self.observer.stop()
 
     def run(self):
+        """
+        Runs the specified observer on the first (and only) thymio node connected via serial until the observer is
+        stopped or interrupted.
+        """
         with self.thymio, self.observer:
             self.thymio.connect()
 
@@ -95,8 +141,4 @@ class SingleSerialThymioRunner:
             id = self.thymio.first_node()
             self.observer.set_thymio_node(self.thymio, id)
 
-            try:
-                self.observer.run()  # blocks until end
-            except PortNotOpenError as error:
-                if not self.observer.done:
-                    raise error  # during disconnect, there may be a serial port error but we don't care about that
+            self.observer.run()  # blocks until done
